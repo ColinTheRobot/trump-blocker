74aebeccf84c54b67160c9363836b1cfa4c1dcb6
Colin Hart <me@colin-hart.com>
2016-11-17 13:34:18 -0500

    add comments

--- chrome_ext/content.js
+++ chrome_ext/content.js
@@ -1,61 +1,30 @@
-// AND SEARCH FOR BACKGROUND IMAGES IN CSS
-  var images = document.getElementsByTagName('IMG');
-  var imageUrls= [];
+var images = document.getElementsByTagName('IMG');
+var imageUrls= [];
 
-  for (let i = 0; i < images.length; i++) {
-    if (images[i].src) {
-      imageUrls.push(images[i].src)
-    } else {
-      imageUrls.push(images[i].getAttribute('data-baseurl'))
-    }
+// GET ALL IMAGES FROM DOM
+for (let i = 0; i < images.length; i++) {
+  if (images[i].src) {
+    imageUrls.push(images[i].src)
+  } else {
+    // TO ACCOUNT FOR OTHER IMAGE ATTR (data-baseurl) at latimes
+    imageUrls.push(images[i].getAttribute('data-baseurl'))
   }
+}
 
-
-// TODO:  CODE TO CHECK FOR BACKGROUND IMAGES
-
-// for (var i in sheets) {
-//   var rules = sheets[i].rules || sheets[i].cssRules
-//   for (var r in rules) {
-//     var json = css2json(rules[r].style)
-//     if (json['background-image'] == "url('http://www.newyorker.com/wp-content/uploads/2016/11/Cassidy-DonaldTrumpsGreatBaitAndSwitch-728x375-1479138921.jpg')") {
-//        console.log(json)
-//     }
-//   }
-// }
-//   function css2json(css) {
-//     var s = {};
-//     if (!css) return s;
-//
-//     if (css instanceof CSSStyleDeclaration) {
-//         for (var i in css) {
-//             if ((css[i]).toLowerCase) {
-//                 s[(css[i]).toLowerCase()] = (css[css[i]]);
-//             }
-//         }
-//     } else if (typeof css == "string") {
-//         css = css.split("; ");
-//         for (var i in css) {
-//             var l = css[i].split(": ");
-//             s[l[0].toLowerCase()] = (l[1]);
-//         }
-//     }
-//     return s;
-// }
-
-// TEST REQUEST
+// TEST REQUEST TO NOT WRECK THE API THROTTLING
 
 // var xhr = new XMLHttpRequest();
 // xhr.onload = function (e) {
 //   let data = JSON.parse(e.target.response)
 //   data.forEach(function(el){
-//     console.log('hey')
-//     var images = document.getElementsByTagName('IMG');
-//     for (var i = 0; i < images.length; ++i) {
-//       if (images[i].src == el.url) {
-//         console.log('TRUEEEE', el.url, images[i])
-//         images[i].setAttribute('src','http://placekitten.com/50/50');
-//       }
-//     }
+//    console.log('hey')
+  // var images = document.getElementsByTagName('IMG');
+  // for (var i = 0; i < images.length; ++i) {
+  //   if (images[i].src == el.url || images[i].getAttribute('data-baseurl') == el.url) {
+  //     console.log('TRUEEEE', el.url, images[i])
+  //     images[i].setAttribute('src','http://placekitten.com/50/50');
+  //   }
+  // }
 //   })
 //
 // };
@@ -67,6 +36,8 @@
   var xhr = new XMLHttpRequest();
   xhr.onload = function (e) {
     let data = JSON.parse(e.target.response)
+
+    // IF THE skybiometry API IS MAXED OUT REPLACE IMAGES THAT CONTAIN TRUMPS NAME
     if (data.status == 500) {
       data.photos.forEach(function(el){
         var images = document.getElementsByTagName('IMG');
@@ -89,7 +60,7 @@
       })
     }
   };
-  
+
   xhr.open('POST', 'http://localhost:3001/photorec', true);
   xhr.setRequestHeader("Content-type", "application/json");
   xhr.send(JSON.stringify({pictures: imageUrls}));
