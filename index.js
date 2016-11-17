dad2eb15bbc0d516d5518f294f4756319414f73d
Colin Hart <me@colin-hart.com>
2016-11-17 13:39:53 -0500

    clean and add more comments

--- index.js
+++ index.js
@@ -15,13 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('./public'))
 
-
 const serviceRoot = 'http://api.skybiometry.com/fc/';
-app.get('/test', (req,res) => {
-  // buildPictureString(req.body.pictures)
-  res.send('hiii hi hi')
-})
-
 
 app.post('/test', (req,res) => {
   // buildPictureString(req.body.pictures)
@@ -33,6 +27,8 @@ app.post('/test', (req,res) => {
   res.json(photosWithTags)
 })
 
+// CURRENTLY ONLY WORKS WITH A SINGLE IMAGE AT A TIME
+// res.json erroring dude to some Scope issue
 app.post('/phototrain', (req, res) => {
     request.get(`${serviceRoot}faces/detect?api_key=${process.env.API_KEY}&api_secret=${process.env.API_SECRET}&urls=${req.body.pictures}`
     , function (error, res, body) {
@@ -41,12 +37,11 @@ app.post('/phototrain', (req, res) => {
 
         var body = JSON.parse(body)
         var tags = "";
-        // eval(pry.it)
         for (var i in body.photos) {
 
             if (body["photos"][i].tags) {
-                //we make the enforcement that the client only sends pictures of people with only 1 face in it.
-                //we do this because we don't want to deal with the complexity of multiple faces in training pictures
+                // we make the enforcement that the client only sends pictures of people with only 1 face in it.
+                // we do this because we don't want to deal with the complexity of multiple faces in training pictures
                 if (body["photos"][i].tags.length > 1) {
                     res.send(400, { message: 'You must send photos with clearly only 1 face in it. This photo has more than one face ' + body["photos"][i].url });
                     return;
@@ -58,13 +53,11 @@ app.post('/phototrain', (req, res) => {
             }
         }
         console.log('Got the  tag ids: ' + tags);
-        // eval(pry.it)
 
         //check if we didn't get any faces at all. If so, error out.
-        // if (tags.length == 0) {
-        //     res.send(400, { message: 'None of the photos you sent had faces in it. ' + body["photos"][i].url });
-        //     // return;
-        // }
+        if (tags.length == 0) {
+            res.send(400, { message: 'None of the photos you sent had faces in it. ' + body["photos"][i].url });
+        }
 
         //save the tags tagged as the name provided
         request.get(serviceRoot + "tags/save?api_key=" + process.env.API_KEY + "&api_secret=" + process.env.API_SECRET + "&uid=" + req.body.name + '@' + 'mainspace' + "&tids=" + tags,
@@ -77,71 +70,38 @@ app.post('/phototrain', (req, res) => {
             request.get(`${serviceRoot}faces/train?api_key=${process.env.API_KEY}&api_secret=${process.env.API_SECRET}&uids=${req.body.name}@mainspace`,
             function (error, res, body) {
 
-                if (error) { return res.send(500, error); }
-
-                for (var i in body["photos"]) {
-                    //api will resize images to max width/height of 1024. However it won't reflect that in width/height
-                    //field of each photo returned. So change it here
-                    var photo = body["photos"][i];
-
-                    //remember, we assume 1 tagged face in each photo
-                    var tag = photo.tags[0];
-
-                    //coordinates of interesting points are returned in percentages and
-                    //need to be re-calculated as absolute positions before rendering
-                    tag.eye_left.x = (tag.eye_left.x / 100) * photo.width;
-                    tag.eye_left.y = (tag.eye_left.y / 100) * photo.height;
-                    tag.eye_right.y = (tag.eye_right.y / 100) * photo.height;
-                    tag.eye_right.x = (tag.eye_right.x / 100) * photo.width;
-                    tag.mouth_center.x = (tag.mouth_center.x / 100) * photo.width;
-                    tag.mouth_center.y = (tag.mouth_center.y / 100) & photo.height;
-                    tag.nose.x = (tag.nose.x / 100) * photo.width;
-                    tag.nose.y = (tag.nose.y / 100) * photo.height;
-
-                }
-                console.log(body)
+              res.json({ data: body });
             });
-            //return the list of images that were sucessfully used for training
         });
-        // res.json({ data: body });
+        //return the list of images that were sucessfully used for training
     });
 });
 
 app.post('/photorec', function (req, res) {
-    var pictureString = req.body.pictures.join(',', ',')
-    console.log(pictureString)
-    request.get(`${serviceRoot}faces/recognize?api_key=${process.env.API_KEY}&api_secret=${process.env.API_SECRET}&uids=all&urls=${pictureString}&namespace=mainspace&detector=aggresive&attributes=none&limit=3`,
+  var pictureString = req.body.pictures.join(',', ',')
+  console.log(pictureString)
+  request.get(`${serviceRoot}faces/recognize?api_key=${process.env.API_KEY}&api_secret=${process.env.API_SECRET}&uids=all&urls=${pictureString}&namespace=mainspace&detector=aggresive&attributes=none&limit=3`,
     function (error, response, body) {
-        var body = JSON.parse(body)
-        // eval(pry.it)
-        if (body.status == 'failure') { return res.json({status: 500, photos: eliminateTrump(pictureString)}); }
+      var body = JSON.parse(body)
 
-        //because the API can return a success error code but not recognize any face we need to
-        //check if we got a picture with a face detected.
-        // else if (!body["photos"][0].tags || !body.photos[0].tags[0]) {
-        //     res.send(400, { message: 'sorry, no photos detected in this image' });
-        // }
+      // IF THE API IS MAXED OUT FILTER OUT URLS WITHOUT TRUMPS NAME
+      if (body.status == 'failure') { return res.json({status: 500, photos: eliminateTrump(pictureString)}); }
 
-        console.log(body)
-        var photosWithTags = body.photos.filter(function(val) {
-          if (val.tags && !val.tags.length == 0 ) {
-            return val
-          }
-        })
-        // eval(pry.it)
+      var photosWithTags = body.photos.filter(function(val) {
+        if (val.tags && !val.tags.length == 0 ) {
+          return val
+        }
+      })
 
-        // FILTER BY CONFIDENCE
-        // var filteredPhotos = photosWithTags.filter(function(val) {
-        //   if (val.tags[0].uids[0].confidence > 40) {
-        //     return val
-        //   }
-        // })
+      // FILTER BY CONFIDENCE (COMMENTED OUT DUE TO LACK OF TRAINING)
+      // var filteredPhotos = photosWithTags.filter(function(val) {
+      //   if (val.tags[0].uids[0].confidence > 40) {
+      //     return val
+      //   }
+      // })
 
-        res.json({ photos: photosWithTags});
-
-      // res.send(400, { message: 'The API has no guesses as to who is in the photo' });
-
-    });
+    res.json({ photos: photosWithTags});
+  });
 });
 
 var buildPictureString = function(images) {
